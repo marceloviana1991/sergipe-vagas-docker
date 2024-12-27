532f5771c2c183d3d281bf802ccb2433e66d84f0
marcelo <marceloviana1991@gmail.com>
2024-12-27 20:31:14 -0300

    create class SecurityConfigurations

--- src/main/java/marceloviana1991/sergipe_vagas/infra/SecurityConfigurations.java
+++ src/main/java/marceloviana1991/sergipe_vagas/infra/SecurityConfigurations.java
@@ -2,21 +2,17 @@ package marceloviana1991.sergipe_vagas.infra;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfigurations {
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        return http.csrf(AbstractHttpConfigurer::disable)
-                .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                .build();
+    public PasswordEncoder encriptador(){
+        return new BCryptPasswordEncoder();
     }
 
 }
