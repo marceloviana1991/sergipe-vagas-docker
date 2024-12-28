72a7cb1358019b2e52a6f3010d99e9d88b90d656
marcelo <marceloviana1991@gmail.com>
2024-12-27 21:52:27 -0300

    restaurando classe de testes

---
+++ src/test/java/marceloviana1991/sergipe_vagas/SergipeVagasApplicationTests.java
@@ -0,0 +1,12 @@
+package marceloviana1991.sergipe_vagas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class SergipeVagasApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
