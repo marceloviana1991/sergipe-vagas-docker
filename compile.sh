00bf8d419efacbd531f16fb259400852734a4a10
marcelo <marceloviana1991@gmail.com>
2024-12-30 10:28:54 -0300

    excluindo dependência do maven

--- compile.sh
+++ compile.sh
@@ -3,7 +3,7 @@
 export DB_HOST="localhost"
 docker run -p 3306:3306 --name mysql-compile -e MYSQL_ROOT_PASSWORD=root -e MYSQL_DATABASE=sergipe-vagas -d mysql
 sleep 5
-mvn install
+./mvnw package
 docker stop mysql-compile
 docker rm mysql-compile
 docker rmi mysql
