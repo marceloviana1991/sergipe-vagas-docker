8a91547e524987983927ccd377debbd4fc83dcb8
marcelo <marceloviana1991@gmail.com>
2025-01-03 11:46:48 -0300

    porta do db em variavel de ambiente

--- compile.sh
+++ compile.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 export DB_HOST="localhost"
+export DB_PORT="3306"
 docker run -p 3306:3306 --name mysql-compile -e MYSQL_ROOT_PASSWORD=root -e MYSQL_DATABASE=sergipe-vagas -d mysql
 sleep 5
 ./mvnw package
